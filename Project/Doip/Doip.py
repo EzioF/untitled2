dc4cb7cb19a0caade196c6165c8e41293859984b
EzioF <415054118@qq.com>
2019-01-30 21:21:52 +0800

    SWDL

--- Project/Doip/Doip.py
+++ Project/Doip/Doip.py
@@ -3,7 +3,7 @@ import os
 import test
 import openpyxl
 
-print('test1111')
+print('test1')
 from openpyxl import Workbook
 from openpyxl.styles import Font, Color
 from openpyxl.styles import colors
